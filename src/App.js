e6149692f2f413e583389eb4d7bdfe54f09e08be
sonalighodake43772 <sonalighodake867>
2023-02-10 09:50:32 +0530

    implementing routes and about page

--- src/App.js
+++ src/App.js
@@ -1,28 +1,37 @@
 import "./App.css";
-import React,{useState,useContext} from "react";
+import React, { useState } from "react";
 import NavBar from "./component/NavBar";
 import Items from "./component/Items";
 import Cart from "./component/Cart/Cart";
 import { CartContextProvider } from "./component/store/cart-context";
+import { Route, Routes } from "react-router-dom";
+import About from "./pages/About";
 
 function App() {
-
-  
   const [showCart, setShowCart] = useState(false);
 
   const CartHandler = () => {
     setShowCart(true);
   };
-  
-  const cartCloseHandler= ()=>{
-    setShowCart(false)
-  }
+
+  const cartCloseHandler = () => {
+    setShowCart(false);
+  };
   return (
     <CartContextProvider>
+      
       <NavBar onshow={CartHandler} />
       <h1 className="text-center p-5  bg-secondary text-white">The Generics</h1>
-      {showCart && <Cart onTap={cartCloseHandler}/>}
-     <Items/>
+      {showCart && <Cart onTap={cartCloseHandler} />}
+      <Routes>
+      <Route path="/home"></Route>
+      <Route path="/store" element={  <Items /> }/>
+       
+    
+      <Route path="/about" element={ <About />}/>
+        
+      
+      </Routes>
     </CartContextProvider>
   );
 }
