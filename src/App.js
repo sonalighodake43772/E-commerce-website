f92c635a84a376f806e960ad505f18115aeb78ae
sonalighodake43772 <sonalighodake867>
2023-02-10 11:26:16 +0530

    implement products page

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import { Route, Routes } from "react-router-dom";
 import About from "./pages/About";
 import Home from "./pages/Home"
 import Contact from "./pages/Contact";
+import Products from "./pages/Products";
 
 function App() {
   const [showCart, setShowCart] = useState(false);
@@ -27,8 +28,12 @@ function App() {
       {showCart && <Cart onTap={cartCloseHandler} />}
       <Routes>
       <Route path="/home" element={<Home/>}/>
-      <Route path="/store" element={  <Items /> }/>
-       
+      <Route path="/store/:title" element={  <Products /> }/>
+      <Route path="/store" element={<Items/>}/>
+         
+        
+        <Route path="/store/:title" element={<Products/>}/>
+          
     
       <Route path="/about" element={ <About />}/>
       <Route path="/contact" element={ <Contact />}/>
