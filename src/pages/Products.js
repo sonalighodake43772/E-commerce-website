3c2d9f3389a573a693374f24813f975dc026e285
sonalighodake43772 <sonalighodake867>
2023-02-10 17:44:22 +0530

    add review in products

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,36 +1,44 @@
 import { Container } from "react-bootstrap";
 import { useParams } from "react-router-dom";
 
+
 const productsArr = [
   {
     title: "Colors",
     price: 100,
     imageUrl: "https://prasadyash2411.github.io/ecom-website/img/Album%201.png",
+    review:4.8
   },
   {
     title: "Black and white Colors",
     price: 50,
     imageUrl: "https://prasadyash2411.github.io/ecom-website/img/Album%202.png",
+    review:3.9
   },
   {
     title: "Yellow and Black Colors",
     price: 70,
     imageUrl: "https://prasadyash2411.github.io/ecom-website/img/Album%203.png",
+    review:3.7
   },
   {
     title: "Blue Color",
     price: 250,
     imageUrl: "https://prasadyash2411.github.io/ecom-website/img/Album%204.png",
+    review:2.8
   },
 ];
 const Products = () => {
+   
   
   const { title } = useParams();
 
   const items = productsArr.find((item) => {
     return item.title === title;
   });
+
   //console.log(items.title);
+ 
 
   return (
     <section>
@@ -39,8 +47,10 @@ const Products = () => {
         <h3>{items.title}</h3>
         <img src={items.imageUrl} alt={items.price}></img>
         <h3>Price: {items.price}</h3>
-        <h1>Reviews</h1>
-        <p> this is a {items.title} review </p>
+        <h2>Reviews -{items.review}</h2>
+       
+
+
       </Container>
     </section>
   );
